b3fe1080fd513288b887c32ab230938f1f40d3c0
tomplt <tom_paulat@web.de>
2023-10-07 23:13:19 +0200

    sql -> csv

---
+++ get_data.sql
@@ -0,0 +1,20 @@
+SELECT 
+    climbs.uuid, climbs.layout_id, climbs.setter_id, climbs.setter_username, climbs.name, 
+    climbs.frames, climbs.edge_bottom, climbs.edge_top, climbs.edge_left, climbs.edge_right,
+    climb_stats.angle, climb_stats.display_difficulty, climb_stats.benchmark_difficulty,
+    beta_links.link, beta_links.foreign_username, beta_links.angle as beta_angle, 
+    climb_cache_fields.ascensionist_count, climb_cache_fields.display_difficulty, climb_cache_fields.quality_average
+FROM 
+    climbs
+JOIN 
+    climb_stats ON climbs.uuid = climb_stats.climb_uuid
+JOIN 
+    beta_links ON climbs.uuid = beta_links.climb_uuid
+JOIN 
+    climb_cache_fields ON climbs.uuid = climb_cache_fields.climb_uuid
+GROUP BY 
+    climbs.uuid
+ORDER BY
+    climb_cache_fields.ascensionist_count
+DESC
+
