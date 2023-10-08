f9ed65172d00ddd3975e5d3cf02f45e2e4bca2a6
tomplt <tom_paulat@web.de>
2023-10-08 19:32:54 +0200

    adding different angels of the climbs

--- get_data.sql
+++ get_data.sql
@@ -1,20 +1,22 @@
 SELECT 
-    climbs.uuid, climbs.layout_id, climbs.setter_id, climbs.setter_username, climbs.name, 
-    climbs.frames, climbs.edge_bottom, climbs.edge_top, climbs.edge_left, climbs.edge_right,
-    climb_stats.angle, climb_stats.display_difficulty, climb_stats.benchmark_difficulty,
-    beta_links.link, beta_links.foreign_username, beta_links.angle as beta_angle, 
-    climb_cache_fields.ascensionist_count, climb_cache_fields.display_difficulty, climb_cache_fields.quality_average climb_stats
+    c.uuid, c.layout_id, c.setter_id, c.setter_username, c.name, 
+    c.frames, c.edge_bottom, c.edge_top, c.edge_left, c.edge_right,
+    cs.angle, cs.display_difficulty, cs.benchmark_difficulty,
+    cs.ascensionist_count, cs.display_difficulty, cs.quality_average,
+    GROUP_CONCAT(bl.link || ',' || bl.foreign_username, ';') AS link_and_username,
+    GROUP_CONCAT(CAST(bl.angle AS TEXT), ';') AS beta_angles
 FROM 
-    climbs
+    climbs c
 JOIN 
-    climb_stats ON climbs.uuid = climb_stats.climb_uuid
+    climb_stats cs ON c.uuid = cs.climb_uuid
+LEFT JOIN 
+    beta_links bl ON c.uuid = bl.climb_uuid
 JOIN 
-    beta_links ON climbs.uuid = beta_links.climb_uuid
-JOIN 
-    climb_cache_fields ON climbs.uuid = climb_cache_fields.climb_uuid
-GROUP BY 
-    climbs.uuid
+    climb_cache_fields cf ON c.uuid = cf.climb_uuid
+GROUP BY
+    c.uuid, c.layout_id, c.setter_id, c.setter_username, c.name, 
+    c.frames, c.edge_bottom, c.edge_top, c.edge_left, c.edge_right,
+    cs.angle, cs.display_difficulty, cs.benchmark_difficulty, 
+    cf.ascensionist_count, cf.display_difficulty, cf.quality_average
 ORDER BY
-    climb_cache_fields.ascensionist_count
-DESC
-
+    cf.ascensionist_count DESC;
